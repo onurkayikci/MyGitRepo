14264eacf04b4f598160968b6d30a5928d8e9050
Onur Kayikci <onurkayikci.ist@gmail.com>
2014-03-06 00:10:34 +0100

    navigation bar und filters to modify data

--- ang-news/app/scripts/app.js
+++ ang-news/app/scripts/app.js
@@ -15,7 +15,14 @@ app.config(['$routeProvider', function($routeProvider){
 		when('/', {
 		templateUrl: 'views/posts.html',
 		controller: 'PostsCtrl'
-	});
+		})
+		.when('/posts/:postId', {
+			templateUrl : 'views/showpost.html',
+			controller : 'PostViewCtrl'
+		})
+		.otherwise({
+			redirectTo : '/'
+		});
 }]);
 
 app.constant('FIREBASE_URL', 'https://dazzling-fire-4978.firebaseio.com/');
--- ang-news/app/scripts/controllers/posts.js
+++ ang-news/app/scripts/controllers/posts.js
@@ -2,13 +2,7 @@
 
 app.controller('PostsCtrl', function ($scope, Post){
 	$scope.posts=Post.all;
-	$scope.post = {url: 'http://', title:''};
-
-	$scope.submitPost = function(){
-		Post.create($scope.post).then(function(){
-			$scope.post = {url: 'http://', title:''};
-		});	
-	};
+	$scope.post = {url: 'http://', title : ''};
 
 	$scope.deletePost = function(postId){
 		Post.delete(postId);
