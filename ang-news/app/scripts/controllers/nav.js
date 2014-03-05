90f24f5ebb7bc93b332359361bad9dbf8c578843
Onur Kayikci <onurkayikci.ist@gmail.com>
2014-03-06 00:15:22 +0100

    Some new views and controllers

---
+++ ang-news/app/scripts/controllers/nav.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.controller('NavCtrl', function ($scope, $location, Post){
+	$scope.posts= {url: 'http://', title: ''};
+
+	$scope.submitPost = function(){
+		Post.create($scope.post).then(function(ref){
+			$location.path('/posts/' + ref.name());
+			$scope.post = {url: 'http://', title: ''};
+		});	
+	};
