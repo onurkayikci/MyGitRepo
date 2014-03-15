b00f1aaf4048680c5b6c6f2d2ebc79cb36e792be
Onur Kayikci <onurkayikci.ist@gmail.com>
2014-03-15 01:07:39 +0100

    Some new views and controllers to enable registering and authorisation

--- ang-news/app/scripts/app.js
+++ ang-news/app/scripts/app.js
@@ -8,7 +8,7 @@ var app = angular.module('angNewsApp', [
 	'ngSanitize',
 	'ngRoute',
 	'firebase'
-	]);
+]);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
@@ -19,6 +19,14 @@ app.config(['$routeProvider', function($routeProvider){
 		.when('/posts/:postId', {
 			templateUrl : 'views/showpost.html',
 			controller : 'PostViewCtrl'
+		})
+		.when('/login', {
+			templateUrl:'views/login.html',
+			controller : 'AuthCtrl'
+		})
+		.when('/register', {
+			templateUrl: 'views/register.html',
+			controller: 'AuthCtrl'
 		})
 		.otherwise({
 			redirectTo : '/'
